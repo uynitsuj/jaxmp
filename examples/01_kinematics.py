2f1ef1145354229dc0fd718d8fcdf93cca22c95f
Chung Min Kim <chungmin99@gmail.com>
2024-09-16 23:17:12 -0700

    Allow urdf_paths as input, allow multiple joints as input

--- examples/01_kinematics.py
+++ examples/01_kinematics.py
@@ -2,8 +2,11 @@
 Tests robot forward + inverse kinematics using JaxMP.
 """
 
+from typing import Optional
+from pathlib import Path
 import time
 import tyro
+import yourdfpy
 
 from robot_descriptions.loaders.yourdfpy import load_robot_description
 
@@ -25,8 +28,14 @@ def main(
     rot_weight: float = 1.0,
     rest_weight: float = 0.01,
     limit_weight: float = 100.0,
+    robot_urdf_path: Optional[Path] = None,
 ):
-    urdf = load_robot_description(robot_description)
+    if robot_urdf_path is not None:
+        def filename_handler(fname: str) -> str:
+            return str(robot_urdf_path.parent / fname)
+        urdf = yourdfpy.URDF.load(robot_urdf_path, filename_handler=filename_handler)
+    else:
+        urdf = load_robot_description(robot_description)
     urdf = sort_joint_map(urdf)
 
     kin = JaxKinTree.from_urdf(urdf)
@@ -37,38 +46,44 @@ def main(
 
     # Visualize robot, target joint pose, and desired joint pose.
     urdf_vis = viser.extras.ViserUrdf(server, urdf)
-    target_tf_handle = server.scene.add_transform_controls("target transform", scale=0.2)
-    target_frame_handle = server.scene.add_frame("target", axes_length=0.1)
+    urdf_vis.update_cfg(onp.array(rest_pose))
     server.scene.add_grid("ground", width=2, height=2, cell_size=0.1)
 
-    # Timing info.
     timing_handle = server.gui.add_number("Time (ms)", 0.01, disabled=True)
 
-    # Show target joint name, and current joint positions.
-    target_name_handle = server.gui.add_dropdown(
-        "target joint",
-        list(urdf.joint_names),
-        initial_value=urdf.joint_names[0]
-    )
+    add_joint_button = server.gui.add_button("Add joint!")
+    target_name_handles: list[viser.GuiDropdownHandle] = []
+    target_tf_handles: list[viser.TransformControlsHandle] = []
+    target_frame_handles: list[viser.FrameHandle] = []
+
+    @add_joint_button.on_click
+    def _(_):
+        # Show target joint name.
+        idx = len(target_name_handles)
+        target_name_handle = server.gui.add_dropdown(
+            f"target joint {idx}",
+            list(urdf.joint_names),
+            initial_value=urdf.joint_names[0]
+        )
+        target_tf_handle = server.scene.add_transform_controls(f"target_transform_{idx}", scale=0.2)
+        target_frame_handle = server.scene.add_frame(f"target_{idx}", axes_length=0.1)
+
+        target_name_handles.append(target_name_handle)
+        target_tf_handles.append(target_tf_handle)
+        target_frame_handles.append(target_frame_handle)
 
     # Create factor graph.
     JointVar = robot_factors.get_var_class(default_val=rest_pose)
 
     @jdc.jit
-    def solve_ik(target_pose: jaxlie.SE3, target_joint_idx: jdc.Static[int]) -> jnp.ndarray:
+    def solve_ik(
+        target_pose: jaxlie.SE3,
+        target_joint_indices: jdc.Static[tuple[int]],
+    ) -> jnp.ndarray:
         joint_vars = [JointVar(id=0)]
 
         start_time = time.time()
-        factors = [
-            jaxls.Factor.make(
-                robot_factors.ik_cost,
-                (
-                    joint_vars[0],
-                    target_pose,
-                    target_joint_idx,
-                    jnp.array([pos_weight] * 3 + [rot_weight] * 3),
-                ),
-            ),
+        factors: list[jaxls.Factor] = [
             jaxls.Factor.make(
                 robot_factors.limit_cost,
                 (
@@ -84,6 +99,20 @@ def main(
                 ),
             ),
         ]
+
+        for idx, target_joint_idx in enumerate(target_joint_indices):
+            factors.append(
+                jaxls.Factor.make(
+                    robot_factors.ik_cost,
+                    (
+                        joint_vars[0],
+                        jaxlie.SE3(target_pose.wxyz_xyz[idx]),
+                        target_joint_idx,
+                        jnp.array([pos_weight] * 3 + [rot_weight] * 3),
+                    ),
+                )
+            )
+
         graph = jaxls.FactorGraph.make(
             factors,
             joint_vars,
@@ -94,6 +123,7 @@ def main(
             initial_vals=jaxls.VarValues.make(joint_vars, [JointVar.default]),
             trust_region=jaxls.TrustRegionConfig(lambda_initial=1.0),
             termination=jaxls.TerminationConfig(gradient_tolerance=1e-5, parameter_tolerance=1e-5),
+            verbose=False,
         )
 
         # Update timing info.
@@ -104,15 +134,31 @@ def main(
         return joints
 
     while True:
-        target_joint_idx = kin.joint_names.index(target_name_handle.value)
-        target_pose = jaxlie.SE3(jnp.array([*target_tf_handle.wxyz, *target_tf_handle.position]))
+        if len(target_name_handles) == 0:
+            time.sleep(0.1)
+            continue
 
-        joints = solve_ik(target_pose, target_joint_idx)
+        target_joint_indices = [
+            kin.joint_names.index(target_name_handle.value)
+            for target_name_handle in target_name_handles
+        ]
+        target_pose_list = [
+            jaxlie.SE3(jnp.array([*target_tf_handle.wxyz, *target_tf_handle.position]))
+            for target_tf_handle in target_tf_handles
+        ]
+
+        target_poses = jaxlie.SE3(jnp.stack([pose.wxyz_xyz for pose in target_pose_list]))
+        joints = solve_ik(
+            target_poses,
+            tuple[int](target_joint_indices)
+        )
 
         urdf_vis.update_cfg(onp.array(joints))
-        T_target_world = kin.forward_kinematics(joints)[target_joint_idx]
-        target_frame_handle.position = onp.array(T_target_world)[4:]
-        target_frame_handle.wxyz = onp.array(T_target_world)[:4]
+
+        for target_frame_handle, target_joint_idx in zip(target_frame_handles, target_joint_indices):
+            T_target_world = kin.forward_kinematics(joints)[target_joint_idx]
+            target_frame_handle.position = onp.array(T_target_world)[4:]
+            target_frame_handle.wxyz = onp.array(T_target_world)[:4]
 
 
 if __name__ == "__main__":
--- examples/03_kin_with_coll.py
+++ examples/03_kin_with_coll.py
@@ -2,7 +2,10 @@
 Similar to 01_kinematics.py, but with collision detection.
 """
 
+from typing import Optional
+from pathlib import Path
 import time
+import yourdfpy
 
 from robot_descriptions.loaders.yourdfpy import load_robot_description
 
@@ -11,6 +14,7 @@ import tyro
 import jax.numpy as jnp
 import jaxlie
 import numpy as onp
+import jax_dataclasses as jdc
 
 import jaxls
 
@@ -28,10 +32,16 @@ def main(
     rot_weight: float = 1.0,
     rest_weight: float = 0.01,
     limit_weight: float = 100.0,
-    coll_weight: float = 2.0,
+    coll_weight: float = 5.0,
     world_coll_weight: float = 100.0,
+    robot_urdf_path: Optional[Path] = None,
 ):
-    urdf = load_robot_description(robot_description)
+    if robot_urdf_path is not None:
+        def filename_handler(fname: str) -> str:
+            return str(robot_urdf_path.parent / fname)
+        urdf = yourdfpy.URDF.load(robot_urdf_path, filename_handler=filename_handler)
+    else:
+        urdf = load_robot_description(robot_description)
     urdf = sort_joint_map(urdf)
 
     robot_coll = RobotColl.from_urdf(
@@ -49,80 +59,99 @@ def main(
 
     # Visualize robot, target joint pose, and desired joint pose.
     urdf_vis = viser.extras.ViserUrdf(server, urdf)
-    target_tf_handle = server.scene.add_transform_controls("target transform", scale=0.2)
-    target_frame_handle = server.scene.add_frame("target", axes_length=0.1)
-
-    # Show target joint name, and current joint positions.
-    visualize_spheres = server.gui.add_checkbox("Show Collbody", initial_value=False)
-    target_name_handle = server.gui.add_dropdown(
-        "target joint",
-        list(urdf.joint_names),
-        initial_value=urdf.joint_names[0]
-    )
+    urdf_vis.update_cfg(onp.array(rest_pose))
+    server.scene.add_grid("ground", width=2, height=2, cell_size=0.1)
 
     # Create ground plane as an obstacle (world collision)!
     obstacle = HalfSpaceColl(jnp.array([0.0, 0.0, 0.0]), jnp.array([0.0, 0.0, 1.0]))
     server.scene.add_mesh_trimesh("ground_plane", obstacle.to_trimesh())
     server.scene.add_grid("ground", width=3, height=3, cell_size=0.1, position=(0.0, 0.0, 0.001))
-    self_coll_value = server.gui.add_number("max. coll dist (self)", 0.0, step=0.01, disabled=True)
-    world_coll_value = server.gui.add_number("max. coll dist (world)", 0.0, step=0.01, disabled=True)
+    self_coll_value = server.gui.add_number("max. coll (self)", 0.0, step=0.01, disabled=True)
+    world_coll_value = server.gui.add_number("max. coll (world)", 0.0, step=0.01, disabled=True)
+    visualize_spheres = server.gui.add_checkbox("Visualize spheres", False)
 
-    # Timing info.
+    # Add GUI elements, to let user interact with the robot joints.
     timing_handle = server.gui.add_number("Time (ms)", 0.01, disabled=True)
+    add_joint_button = server.gui.add_button("Add joint!")
+    target_name_handles: list[viser.GuiDropdownHandle] = []
+    target_tf_handles: list[viser.TransformControlsHandle] = []
+    target_frame_handles: list[viser.FrameHandle] = []
+
+    @add_joint_button.on_click
+    def _(_):
+        # Show target joint name.
+        idx = len(target_name_handles)
+        target_name_handle = server.gui.add_dropdown(
+            f"target joint {idx}",
+            list(urdf.joint_names),
+            initial_value=urdf.joint_names[0]
+        )
+        target_tf_handle = server.scene.add_transform_controls(f"target_transform_{idx}", scale=0.2)
+        target_frame_handle = server.scene.add_frame(f"target_{idx}", axes_length=0.1)
+
+        target_name_handles.append(target_name_handle)
+        target_tf_handles.append(target_tf_handle)
+        target_frame_handles.append(target_frame_handle)
 
     # Create factor graph.
     JointVar = robot_factors.get_var_class(default_val=rest_pose)
 
     collbody_handle = None
-    def solve_ik():
-        nonlocal collbody_handle
+    @jdc.jit
+    def solve_ik(
+        target_pose: jaxlie.SE3,
+        target_joint_indices: jdc.Static[tuple[int]],
+    ) -> jnp.ndarray:
         joint_vars = [JointVar(id=0)]
 
-        target_joint_idx = kin.joint_names.index(target_name_handle.value)
-        target_pose = jaxlie.SE3(jnp.array([*target_tf_handle.wxyz, *target_tf_handle.position]))
-
         start = time.time()
-        graph = jaxls.FactorGraph.make(
-            [
+        factors: list[jaxls.Factor] = [
+            jaxls.Factor.make(
+                robot_factors.limit_cost,
+                (
+                    joint_vars[0],
+                    jnp.array([limit_weight] * kin.num_actuated_joints),
+                ),
+            ),
+            jaxls.Factor.make(
+                robot_factors.self_coll_cost,
+                (joint_vars[0], 0.01, jnp.array([coll_weight] * len(robot_coll))),
+            ),
+            jaxls.Factor.make(
+                robot_factors.world_coll_cost,
+                (
+                    joint_vars[0],
+                    obstacle,
+                    0.05,
+                    jnp.array([world_coll_weight] * len(robot_coll)),
+                ),
+            ),
+            jaxls.Factor.make(
+                robot_factors.rest_cost,
+                (
+                    joint_vars[0],
+                    jnp.array([rest_weight] * kin.num_actuated_joints),
+                ),
+            ),
+        ]
+        for idx, target_joint_idx in enumerate(target_joint_indices):
+            factors.append(
                 jaxls.Factor.make(
                     robot_factors.ik_cost,
                     (
                         joint_vars[0],
-                        target_pose,
+                        jaxlie.SE3(target_pose.wxyz_xyz[idx]),
                         target_joint_idx,
                         jnp.array([pos_weight] * 3 + [rot_weight] * 3),
                     ),
-                ),
-                jaxls.Factor.make(
-                    robot_factors.limit_cost,
-                    (
-                        joint_vars[0],
-                        jnp.array([limit_weight] * kin.num_actuated_joints),
-                    ),
-                ),
-                jaxls.Factor.make(
-                    robot_factors.self_coll_cost,
-                    (joint_vars[0], 0.05, jnp.array([coll_weight] * len(robot_coll))),
-                ),
-                jaxls.Factor.make(
-                    robot_factors.world_coll_cost,
-                    (
-                        joint_vars[0],
-                        obstacle,
-                        0.01,
-                        jnp.array([world_coll_weight] * len(robot_coll)),
-                    ),
-                ),
-                jaxls.Factor.make(
-                    robot_factors.rest_cost,
-                    (
-                        joint_vars[0],
-                        jnp.array([rest_weight] * kin.num_actuated_joints),
-                    ),
-                ),
-            ],
+                )
+            )
+
+        graph = jaxls.FactorGraph.make(
+            factors,
             joint_vars,
             verbose=False,
+            use_onp=False,
         )
         solution = graph.solve(
             initial_vals=jaxls.VarValues.make(joint_vars, [JointVar.default]),
@@ -135,14 +164,33 @@ def main(
         timing_handle.value = (time.time() - start) * 1000
 
         joints = solution[joint_vars[0]]
-        T_target_world = jaxlie.SE3(  # pylint: disable=invalid-name
-            kin.forward_kinematics(joints)[target_joint_idx]
-        ).wxyz_xyz
+        return joints
 
-        # Update visualization.
+    while True:
+        if len(target_name_handles) == 0:
+            time.sleep(0.1)
+            continue
+
+        target_joint_indices = [
+            kin.joint_names.index(target_name_handle.value)
+            for target_name_handle in target_name_handles
+        ]
+        target_pose_list = [
+            jaxlie.SE3(jnp.array([*target_tf_handle.wxyz, *target_tf_handle.position]))
+            for target_tf_handle in target_tf_handles
+        ]
+
+        target_poses = jaxlie.SE3(jnp.stack([pose.wxyz_xyz for pose in target_pose_list]))
+        joints = solve_ik(target_poses, tuple[int](target_joint_indices))
+
         urdf_vis.update_cfg(onp.array(joints))
-        target_frame_handle.position = onp.array(T_target_world)[4:]
-        target_frame_handle.wxyz = onp.array(T_target_world)[:4]
+
+        for target_frame_handle, target_joint_idx in zip(target_frame_handles, target_joint_indices):
+            T_target_world = kin.forward_kinematics(joints)[target_joint_idx]
+            target_frame_handle.position = onp.array(T_target_world)[4:]
+            target_frame_handle.wxyz = onp.array(T_target_world)[:4]
+
+        urdf_vis.update_cfg(onp.array(joints))
 
         coll = robot_coll.transform(jaxlie.SE3(kin.forward_kinematics(joints)))
         self_coll_value.value = dist_signed(coll, coll).max().item()
@@ -154,10 +202,6 @@ def main(
             )
         elif collbody_handle is not None:
             collbody_handle.remove()
-
-    while True:
-        solve_ik()
-        time.sleep(0.01)
 
 
 if __name__ == "__main__":
