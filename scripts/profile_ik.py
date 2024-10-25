d89ab7dd7234fb93805f1d711d15e449bb118148
Chung Min Kim <chungmin99@gmail.com>
2024-10-24 21:19:34 -0700

    Add to profiler_ik: collisions, n_trials.

--- scripts/profile_ik.py
+++ scripts/profile_ik.py
@@ -10,6 +10,7 @@ import jaxlie
 from loguru import logger
 
 from jaxmp import JaxKinTree
+from jaxmp.coll import RobotColl
 from jaxmp.extras import load_urdf, solve_ik
 import tyro
 
@@ -23,18 +24,20 @@ ROBOT_EE_JOINTS = {
 def profile_ik(
     robot_description: str,
     ee_joint_name: str,
-    batch_size: int,
+    batch_size_list: list[int],
     device: Literal["cpu", "gpu"],
     solver_type: Literal["conjugate_gradient", "dense_cholesky", "cholmod"],
-) -> tuple[float, jax.Array]:
+    with_collision: bool,
+    n_trials: int,
+):
     jax.config.update("jax_platform_name", device)
 
     urdf = load_urdf(robot_description)
     kin = JaxKinTree.from_urdf(urdf)
+    coll = RobotColl.from_urdf(urdf) if with_collision else None
     rest_pose = (kin.limits_upper + kin.limits_lower) / 2
 
     random_key = jax.random.PRNGKey(0)
-    random_poses = jaxlie.SE3.sample_uniform(random_key, batch_axes=(batch_size,))
 
     joint_idx = jnp.array([kin.joint_names.index(ee_joint_name)])
 
@@ -45,38 +48,47 @@ def profile_ik(
             joint_idx,
             rest_pose,
             solver_type=solver_type,
+            robot_coll=coll,
         ),
     )
 
-    # With JIT compile + run.
-    start = time.time()
-    _, joints = solve_ik_vmap(random_poses)
-    jax.block_until_ready((_, joints))
-    end = time.time()
-    elapsed_with_jit = end - start
+    for batch_size in batch_size_list:
+        random_poses = jaxlie.SE3.sample_uniform(random_key, batch_axes=(batch_size,))
 
-    # Without JIT compile.
-    start = time.time()
-    _, joints = solve_ik_vmap(random_poses)
-    jax.block_until_ready((_, joints))
-    end = time.time()
-    elapsed = end - start
+        # With JIT compile + run.
+        start = time.time()
+        _, joints = solve_ik_vmap(random_poses)
+        jax.block_until_ready((_, joints))
+        end = time.time()
+        elapsed_with_jit = end - start
 
-    Ts_solved = kin.forward_kinematics(joints)[..., joint_idx, :]
-    errors = jaxlie.SE3.log(jaxlie.SE3(Ts_solved) @ random_poses.inverse())
+        # Without JIT compile.
+        elapsed = 0
+        for _ in range(n_trials):
+            random_poses = jaxlie.SE3.sample_uniform(random_key, batch_axes=(batch_size,))
+            start = time.time()
+            _, joints = solve_ik_vmap(random_poses)
+            jax.block_until_ready((_, joints))
+            end = time.time()
+            elapsed += end - start
+        elapsed /= n_trials
 
-    logger.info(
-        "For IK on {} with {} samples, with {} DoF:".format(
-            device, batch_size, kin.num_actuated_joints
+        Ts_solved = kin.forward_kinematics(joints)[..., joint_idx, :]
+        errors = jaxlie.SE3.log(jaxlie.SE3(Ts_solved) @ random_poses.inverse())
+
+        logger.info(
+            "For IK on {} with {} samples, with {} DoF{}:".format(
+                device,
+                batch_size,
+                kin.num_actuated_joints,
+                ", with collision" if with_collision else "",
+            )
         )
-    )
-    logger.info(
-        f"\tElapsed time (seconds): {elapsed:10.6f} ({elapsed_with_jit:.6f} with JIT)"
-    )
-    logger.info(f"\tMean translation error: {errors[..., :3].mean():10.6f}")
-    logger.info(f"\tMean rotation error:    {errors[..., 3:].mean():10.6f}")
-
-    return elapsed, errors
+        logger.info(
+            f"\tElapsed time (seconds): {elapsed:10.6f} ({elapsed_with_jit:.6f} with JIT)"
+        )
+        logger.info(f"\tMean translation error: {errors[..., :3].mean():10.6f}")
+        logger.info(f"\tMean rotation error:    {errors[..., 3:].mean():10.6f}")
 
 
 def profile(
@@ -85,20 +97,24 @@ def profile(
     solver_type: Literal[
         "conjugate_gradient", "dense_cholesky", "cholmod"
     ] = "conjugate_gradient",
+    with_collision: bool = False,
 ):
     logger.disable("jaxmp")
     logger.disable("jaxls")
 
     ee_joint_name = ROBOT_EE_JOINTS[robot]
+    batch_size_list = [1, 10, 100, 1000]
+    n_trials = 10
 
-    for batch_size in [1, 10, 100, 1000]:
-        elapsed, errors = profile_ik(
-            robot_description=robot,
-            ee_joint_name=ee_joint_name,
-            batch_size=batch_size,
-            device=device,  # type: ignore
-            solver_type=solver_type,  # type: ignore
-        )
+    profile_ik(
+        robot_description=robot,
+        ee_joint_name=ee_joint_name,
+        batch_size_list=batch_size_list,
+        device=device,  # type: ignore
+        solver_type=solver_type,  # type: ignore
+        with_collision=with_collision,
+        n_trials=n_trials,
+    )
 
 
 if __name__ == "__main__":
